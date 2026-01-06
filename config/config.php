3efd317c356d07bb3e96cd4caaf3f6595ce2323d
hahahihiicha <annisanurulfaizahh@gmail.com>
2026-01-06 19:59:50 +0800

    ganti db

--- config/config.php
+++ config/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
   'db' => [
     'host' => 'localhost',
-    'name' => 'db_lostfound',
+    'name' => 'lostfound',
     'user' => 'root',
     'pass' => '',
     'charset' => 'utf8mb4',
